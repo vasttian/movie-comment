b4423b182604af1fbfb8a59ca2512b8517e1c2ca
vasttian <tianyiming26@gmail.com>
2016-11-12 18:03:54 +0800

    change the grid system with movie-manage page

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,27 +1,27 @@
 $(function() {
 	$(".del").click(function(e) {
-		var target=$(e.target);
-		var id=target.data("id");
-		var tr=$(".item-id-"+id);
+		var target = $(e.target);
+		var id = target.data("id");
+		var tr = $(".item-id-"+id);
 		$.ajax({
 			type: 'DELETE',
 			url: "/admin/movie/list?id=" +id,
 			async: true,
-			success:function(req){
-				if(tr.length>0){
+			success: function(req) {
+				if (tr.length>0) {
 					tr.remove();
 				}
 			},
-			error:function(){
+			error: function() {
 				alert("出错");
 			}
 		})
 	});
 
-	$("#douban").blur(function(){
-		var douban =$(this);
-		var id=douban.val();
-		if(id){
+	$("#douban").blur(function() {
+		var douban = $(this);
+		var id = douban.val();
+		if (id) {
 			$.ajax({
 				url:"https://api.douban.com/v2/movie/subject/" + id,
 				cache:true,
@@ -29,7 +29,7 @@ $(function() {
 				dataType:"jsonp",
 				crossDomain:true,
 				jsonp:"callback",
-				success:function(data){
+				success: function(data) {
 					$("#inputTitle").val(data.title);
 					$("#inputDoctor").val(data.directors[0].name);
 					$("#inputCountry").val(data.countries[0]);
