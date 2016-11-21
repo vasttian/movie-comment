8ee3a71ef09980783a8319ad73a86517861aa93c
tianyiming <tianyiming26@gmail.com>
2016-11-21 15:08:57 +0800

    fix

--- app/controllers/invitation-code.js
+++ app/controllers/invitation-code.js
@@ -6,10 +6,14 @@ exports.checkInvitationCode = function(req, res) {
   console.log("检查待注册的邀请码是否正确");
 	// console.log('_user',_user);
 
+	//db.invitationcodes.insert({code: "movieadmin"})
+	//db.invitationcodes.insert({code: "useradminmovieadmin"})
+	//db.invitationcodes.insert({code: "superadmin"})
   InvitationCode.findOne({code: _user.invitationCode}, function(err, code) {
 	  if (err) {
 	    console.log('err');
 	  };
+	  console.log("code:", code);
 	  if (code) {
 	    console.log('邀请码填写正确!');
 	    return res.json({"valid": true});
