cf08a1bd201e0a42659187658cd911354f8ce481
tianyiming <tianyiming26@gmail.com>
2017-02-06 20:44:21 +0800

    add label to movie categories column chart

--- public/js/view/active-admin-view.js
+++ public/js/view/active-admin-view.js
@@ -172,6 +172,23 @@ $(function() {
 			        name: '电影分类',
 			        type: 'bar',
 			        data: seriesData,
+			        itemStyle: {
+			        	normal: {
+			        		color: function(params) {
+                      var colorList = [
+                        '#C1232B','#B5C334','#FCCE10','#E87C25','#27727B',
+                         '#FE8463','#9BCA63','#FAD860','#F3A43B','#60C0DD',
+                         '#D7504B','#C6E579','#F4E001','#F0805A','#26C0C0'
+                      ];
+                      return colorList[params.dataIndex]
+                  },
+                  label: {
+                      show: true,
+                      position: 'top',
+                      formatter: '{b}\n{c}'
+                  }
+			        	}
+			        }
 			      }],
 			    };
 
