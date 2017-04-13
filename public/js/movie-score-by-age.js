b1267bff1f9e89d53ce0eb3c473eded078098597
tianyiming <tianyiming26@gmail.com>
2017-04-13 16:15:19 +0800

    add movie-score-by-age.ejs, dnf

---
+++ public/js/movie-score-by-age.js
@@ -0,0 +1,8 @@
+$(function() {
+	var colorListArray = [
+		'#C1232B','#B5C334','#FCCE10','#E87C25','#27727B',
+    '#FE8463','#9BCA63','#FAD860','#F3A43B','#60C0DD',
+    '#D7504B','#C6E579','#F4E001','#F0805A','#26C0C0'
+  ];
+  // console.log("movie-score-by-age: ", movie)
+});
