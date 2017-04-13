cf8b43671b8fd7071a2adeb024ed0e513580eef4
tianyiming <tianyiming26@gmail.com>
2017-04-13 14:10:03 +0800

    fix some files code style

--- public/js/user.js
+++ public/js/user.js
@@ -1,16 +1,17 @@
 $(function() {
 
-	$(".del").click(function(e) {
+	$('.del').click(function(e) {
 		var target = $(e.target);
-		var id = target.data("id");
-		var tr = $(".item-id-" + id);;
+		var id = target.data('id');
+		var tr = $('.item-id-' + id);;
 		var data = {
 			id: id
 		};
+
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			data: data,
-			url: "/admin/del/user",
+			url: '/admin/del/user',
 			async: true,
 			success: function(data) {
 				if (data.status == 1) {
@@ -21,36 +22,38 @@ $(function() {
 			},
 			error: function() {
 				console.log("删除用户失败!");
-			}
+			},
 		});
 	});
 
-	$(".updatePower").click(function(e) {
-		var num = $("#role").val();
-		var id = $("#userId").val();
+	$('.updatePower').click(function(e) {
+		var num = $('#role').val();
+		var id = $('#userId').val();
 		var data = {
 			id: id,
-			role: num
+			role: num,
 		};
+
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			data: data,
-			url: "/admin/update/role",
+			url: '/admin/update/role',
 			async: true,
 			success: function(data) {
 				if (data.status == 1) {
-					$("#updateUserPower").modal('hide');
+					$('#updateUserPower').modal('hide');
 				}
 			},
 			error: function() {
-				console.log("更新用户权限失败!");
+				console.log('更新用户权限失败!');
 			}
 		});
 	});
 
-	$(".update").click(function(e) {
+	$('.update').click(function(e) {
 		var target = $(e.target);
-		var id = target.data("id");
-		$("#userId").val(id);
+		var id = target.data('id');
+
+		$('#userId').val(id);
 	});
 });
